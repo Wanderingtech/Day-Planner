5bceece071f613fa72a9f2835afa0626c3decf81
Wanderingtech <heather86lyn@gmail.com>
2020-06-02 18:43:08 -0700

    Pseudo code JS

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+//User enters event by clicking a timeblock
+//Saves to local storage
+//Prevent default to keep events after page refresh
+//current day at top
+//Timeblocks for each hour of a day. Work day 8-5
+//Color coded blocks for past/present/future
