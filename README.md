98ed12381c86c38614d1a9e0703801a0e1c05eea
Wanderingtech <heather86lyn@gmail.com>
2020-06-03 22:25:24 -0700

    minimal  JS and update README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Day-Planner
 Wk 5 HW. A daily planner/scheduler
+## Goals
+* To create a daily planner based off average work day
+
+* Have timeblocks for input of tasks
+
+* Color code the tasks for past/present/future
+
+* Have the event save to local storage
+
+* Prevent the data from being cleared on refresh
+
+## Improvements
+This is completely unfinished. I struggled with whether it was preferred to create the elements through JQuery or to edit the HTML. Also, I found it difficult to know what kind of element I was creating based off reading the CSS and HTML given. I could understand the class vs ID but then when it came to input or other type, I felt lost.
+
+Will continue to work through this and resubmit a finished project.
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
-//User enters event by clicking a timeblock
-//Saves to local storage
-//Prevent default to keep events after page refresh
 //current day at top
+$("#currentDay").text(moment().format("dddd MMM Do YYYY"))
+//Adding the elements to html
+$(".container").append("<input class='description' type='text'></input>")
+$(".container").append("<input class='hour' type='text'></input>");
+$(".container").append("<input class='row' type=text></div>");
+$(".container").append("<input class='time-block' type=text></input>");
+$(".container").append("<button class='saveBtn'>SAVE</button>");
+
+//User enters event by clicking a timeblock
+
+
+//Prevent default to keep events after page refresh
+
+
 //Timeblocks for each hour of a day. Work day 8-5
+
+
 //Color coded blocks for past/present/future
