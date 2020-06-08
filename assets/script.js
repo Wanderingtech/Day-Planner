2074458f40b9098b25f2c3e990c04a06e45ff839
Wanderingtech <heather86lyn@gmail.com>
2020-06-07 22:37:24 -0700

    JS update

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,68 @@
-//Adding the elements to html. Decided directly changing html worked better for now
-// $(".container").append("<div class='row'></div>");
-// $(".row").append("<input class='time-block col-md-8'></input>");
-// $(".time-block").append("<input class='description' type='text'></input>")
-// $(".row").append("<button class='btn saveBtn col-md-1'><i class='fas fa-save'></i></button>");
-
 //current day at top
 $("#currentDay").text(moment().format("dddd MMM Do YYYY"))
-
-//Timeblocks for each hour of a day. Work day 8-5
-
-
-//User enters event by clicking a timeblock
+//Current hours
+var currentHour = moment().hours();
 
 
-//Prevent default to keep events after page refresh
 
+//User hits save after entering a task
+$(".saveBtn").on("click", function(){
+
+    event.preventDefault();
+    
+    //Gets values and saves to local storage
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, value);
+
+    console.log(time, value);
+    console.log(localStorage);
+
+})
 
 //Color coded blocks for past/present/future
+//Loops through timeblocks
+function hourColor() {
+
+    $(".time-block").each(function(){
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+        //Check hours if they're in the past, present, or future
+        if(blockHour < currentHour) {
+            //Changes color of time-block if in the past
+            // $(this).addClass("past");
+            $(this).children("textarea").addClass("past");
+        }
+        else if (blockHour === currentHour) {
+            //Changes color if the time-block is in the present
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+            //Changes color if the time-block is in the future
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+})}
+
+hourColor();
+
+// Load updates from local storage at page load
+$("#hour-8.description").val(localStorage.getItem("hour-8"));
+$("#hour-9.description").val(localStorage.getItem("hour-9"));
+$("#hour-10.description").val(localStorage.getItem("hour-10"));
+$("#hour-11.description").val(localStorage.getItem("hour-11"));
+$("#hour-12.description").val(localStorage.getItem("hour-12"));
+$("#hour-13.description").val(localStorage.getItem("hour-13"));
+$("#hour-14.description").val(localStorage.getItem("hour-14"));
+$("#hour-15.description").val(localStorage.getItem("hour-15"));
+$("#hour-16.description").val(localStorage.getItem("hour-16"));
+$("#hour-17.description").val(localStorage.getItem("hour-17"));
+
+
+// Load the localStorage values
+// for(let i = 8; i < 18; i++){
+//     $(`#hour-${i}.description`).val(localStorage.getItem(`hour-${i}`));
+// }
